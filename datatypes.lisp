56fb5735309596f17dd1e71d0725c89f83a1c349
Ryan Barry <ryan@nuclearice.com>
2015-01-29 02:53:38 -0800

    add method for building ipv4 addr from dotted quad

--- datatypes.lisp
+++ datatypes.lisp
@@ -118,10 +118,27 @@
                  :len (length str)
                  :str str))
 
-(defun build-ip-addr (&rest addr)
-  "make a 16-byte address in network byte order"
+(defgeneric build-ip-addr (addr)
+  (:documentation "make a 16-byte address in network byte order"))
+
+(defmethod build-ip-addr ((addr string))
+  (let ((result 0))
+    (setf (ldb (byte 8 80) result) 255) ;;;; 0xFFFF to represent IPv4
+    (setf (ldb (byte 8 88) result) 255) ;;;; address within IPv6 address
+    (loop with lowbit = 96
+       with byte = 0
+       for char across addr
+       do (if (char= char #\.)
+              (progn (setf (ldb (byte 8 lowbit) result) byte)
+                     (incf lowbit 8)
+                     (setf byte 0))
+              (setf byte (+ (- (char-int char) 48) (* 10 byte))))
+       finally (setf (ldb (byte 8 lowbit) result) byte))
+    result)))
+
+(defmethod build-ip-addr (addr)
   (if (or (< (length addr) 4) (some #'> addr '(255 255 255 255)))
-      (error "You must specify the address as 4 numbers, e.g. 192 168 1 10")
+      (error "You must specify the address as a list of 4 numbers, e.g. 192 168 1 10")
       (let ((result 0))
         (setf (ldb (byte 8 80) result) 255) ;;;; 0xFFFF to represent IPv4
         (setf (ldb (byte 8 88) result) 255) ;;;; address within IPv6 address
